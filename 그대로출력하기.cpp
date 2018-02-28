b317b01adce428de47b36c14c385f2a841050882
repo7 <liberty7479@naver.com>
2018-02-28 11:19:07 +0900

    Create 그대로출력하기.cpp
    fgets(s, 100, stdin);
    scanf("%d[^\n]\n", s); // 줄바꿈을 입력받지 않는다, 앞뒤 공백 무시하고 입력받음, 문자부터 입력받는다. 
    getline(cin, s);
    위의 세 방법은 모두 한 줄을 전체로 입력받을 수 있다. 
    fegets는 줄바꿈까지 입력받는다.
    
    ※scanf(%1d", &s);  
    에서 12345 입력하면 1,2,3,4,5따로 입력받는다. (그 길이만큼 입력받는다.)

---
+++ 그대로출력하기.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+int main() {
+
+    char c;
+
+    while ((c = getchar()) && c != EOF) {
+
+        printf("%c",c);
+
+    }
+
+    return 0;
+
+}
+/*
+#include <cstdio>
+
+char s[111];
+
+int main() {
+
+    while (scanf("%[^\n]\n",s)==1) {
+
+        printf("%s\n",s);
+
+    }
+
+    return 0;
+
+}
+*/
